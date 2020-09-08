e2b3f1380b9c88347444421e1b4af3e45b13f731
tranducduy7520 <tranducduy7520@gmail.com>
2020-09-08 09:29:53 +0700

    remove garbage

--- async/index.js
+++ async/index.js
@@ -1,9 +1,9 @@
 var fs = require('fs');
 var path = require('path');
 
-var txtIntro = fs.readFileSync('./module/intro.txt', {encoding: 'utf8'});
+var txtIntro = fs.readFileSync('./async/intro.txt', {encoding: 'utf8'});
 console.log(txtIntro);
 
-fs.writeFileSync('./module/outro.txt', "Hi World!");
+fs.writeFileSync('./async/outro.txt', "Hi World!");
 
 console.log(path.extname('test.doc'));
