a4aaf438cb543004b89772e102b62ef7918fe90f
tranducduy7520 <tranducduy7520@gmail.com>
2020-09-10 21:40:10 +0700

    'update'

--- test.js
+++ test.js
@@ -1,23 +1,42 @@
-var students = [
-    {
-        name: 'John',
-        age: 18
-    },
-    {
-        name: 'Peter',
-        age: 20
-    },
-    {
-        name: 'Marry',
-        age: 17
-    },
-    {
-        name: 'Jack',
-        age: 19
-    },
-    {
-        name: 'Michael',
-        age: 18
-    }
-];
-console.log(JSON.stringify(students));
+var url = 'localhost:3000/students';
+var item = {
+    name: 'Poo',
+    age: 13
+}
+
+//GET - lấy dữ liệu từ url
+axios.get(url).then(function (res) {
+    console.log(res.data);
+}).catch(function (err) {
+    console.log(err.reason);
+})
+
+//POST - gửi dữ liệu lên server
+
+axios.post(url, item).then(function (res) {      //automatic generate next id 
+    console.log(res.data);
+}).catch(function (err) {
+    console.log(err);
+})
+
+//PUT - thay thế object 
+axios.put(url + '/1', item).then(function (res) {
+    console.log(res.data);
+}).catch(function (err) {
+    console.log(err);
+})
+
+//PATCH - cập nhật object
+axios.patch(url + '/3', item).then(function (res) {
+    console.log(res.data);
+}).catch(function (err) {
+    console.log(err);
+})
+
+//DELETE - xóa object
+axios.delete(url + '/2').then(function (res) {
+    console.log(res.data);
+}).catch(function (err) {
+    console.log(err);
+})
+
